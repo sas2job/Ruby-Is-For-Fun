24bfa09b32fee4285d89acd943e9433fde5f5f8d
Aleksandr Smirnov <sas2job@gmail.com>
2018-10-27 12:35:20 +0300

    Added tasks 28-31

--- code/app29.rb
+++ code/app29.rb
@@ -1,13 +1,3 @@
-number = rand(1..10)
-print 'Привет! Я загадал число от 1 до 10, попробуйте угадать: '
-loop do
-  input = gets.to_i
-  if input == number
-  puts 'Правильно!'
-  exit
-end
-
-  if input != number
-  print 'Неправильно, попробуйте еще раз: '
-  end
-end
+# Напишите программу, которая будет выводить случайное число от 500 до 510.
+random = rand(500..510)
+puts "#{random}"
---
+++ code/app32-2.rb
@@ -0,0 +1,11 @@
+number = rand(1..10)
+print 'Привет! Я загадал число от 1 до 10, попробуйте угадать: '
+loop do
+  input = gets.to_i
+  if input == number
+    puts 'Правильно!'
+    exit
+    else
+    print 'Неправильно, попробуйте еще раз: '
+  end
+end
