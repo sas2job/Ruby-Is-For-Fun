692d81bba0f027eb2b34f313e18ef7d460ee62b7
Aleksandr Smirnov <sas2job@gmail.com>
2018-12-12 19:19:54 +0300

    Added task b.04 from 'w3resource'

---
+++ code/w3resource/b.04.rb
@@ -0,0 +1,9 @@
+# encoding: utf-8
+# Write a Ruby program to create a new string which is n copies of a given string where n is a non-negative integer.
+
+print "Input the radius of the circle: "
+radius = gets.to_f
+perimeter = 2 * Math::PI * radius
+puts "The perimeter is #{perimeter}."
+area = Math::PI * radius * radius
+puts "The area is #{area}."
