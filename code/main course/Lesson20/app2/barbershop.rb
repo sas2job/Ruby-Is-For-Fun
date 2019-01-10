cb5c09fc54163d6829db59b52c38c157c3dfd609
Aleksandr <sas2job@gmail.com>
2019-01-10 10:29:57 +0300

    Added home work for Lesson20

--- code/main course/Lesson20/app2/barbershop.rb
+++ code/main course/Lesson20/app2/barbershop.rb
@@ -13,8 +13,28 @@ post '/' do
   @message = "Dear #{@user_name}, we'll be waiting for you at #{@date_time}"
 
   f = File.open 'users.txt', 'a'
-  f.write "User: #{@user_name}, Phone: #{@phone}, Date and Time: #{@date_time}"
+  f.write "User: #{@user_name}, Phone: #{@phone}, Date and Time: #{@date_time}.\n"
   f.close
 
   erb :message
+end
+
+# Добавить зону /admin где по паролю будет выдаваться список тех, кто записался (из users.txt)
+
+get '/admin' do
+  erb :admin
+end
+
+post '/admin' do
+	@login = params[:login]
+	@password = params[:password]
+
+	# провека логина и пароля
+	if @login == 'admin' && @password == 'qwerty'
+  	@file = File.open("./users.txt","r")
+  	erb :watch_result
+	 	else
+		@report = '<p>Доступ запрещён! Неправильный логин или пароль.</p>'
+		erb :admin
+	end
 end
