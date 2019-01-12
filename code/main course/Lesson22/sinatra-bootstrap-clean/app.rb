6045709f9e3da2c160fb3aa0a3bb22478ff234be
Aleksandr <sas2job@gmail.com>
2019-01-12 21:50:32 +0300

    Added Lesson 22

--- code/main course/Lesson22/sinatra-bootstrap-clean/app.rb
+++ code/main course/Lesson22/sinatra-bootstrap-clean/app.rb
@@ -6,3 +6,15 @@ require 'sinatra/reloader'
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"	
 end
+
+get '/about' do
+	erb :about
+end
+
+get '/visit' do
+	erb :visit
+end
+
+get '/contacts' do
+	erb :contacts
+end
