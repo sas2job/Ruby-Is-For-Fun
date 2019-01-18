cf1c48642752e07c4f8f332229192d4a601eb698
Aleksandr <sas2job@gmail.com>
2019-01-18 20:13:50 +0300

    finished Lesson 25

--- code/main course/Lesson25/app.rb
+++ code/main course/Lesson25/app.rb
@@ -2,3 +2,11 @@ require 'sqlite3'
 
 # подключение базы данных
 db = SQLite3::Database.new 'test.sqlite'
+# db.execute "INSERT INTO Cars (Name, Price) VALUES ('Jaguar', 7777)"
+
+db.execute "SELECT * FROM Cars" do |car|
+puts car
+puts "="*10
+end
+
+db.close
