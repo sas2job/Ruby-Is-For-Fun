11fb86b9a8b3e99ad724d42c45aad3b3ca37161c
Aleksandr <sas2job@gmail.com>
2019-01-29 21:36:18 +0300

    Create table Posts

--- code/main course/Lesson28/leprosorium/app.rb
+++ code/main course/Lesson28/leprosorium/app.rb
@@ -13,6 +13,16 @@ before do
 	init_db
 end
 
+configure do
+	init_db
+	@db.execute 'CREATE TABLE IF NOT EXISTS Posts
+	(
+		id INTEGER PRIMARY KEY AUTOINCREMENT, 
+		created_date DATE, 
+		content TEXT
+		)'
+end
+
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
