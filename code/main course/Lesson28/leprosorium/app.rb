18c233ee112475217f60d2d7c07f4116e919efce
Aleksandr <sas2job@gmail.com>
2019-01-30 15:26:10 +0300

    Add comments

--- code/main course/Lesson28/leprosorium/app.rb
+++ code/main course/Lesson28/leprosorium/app.rb
@@ -9,11 +9,17 @@ def init_db
 	@db.results_as_hash = true
 end
 
+# before вызывается каждый раз при перезагрузке любой страницы
 before do
+	# инициализация БД
 	init_db
 end
 
+# configure вызывается каждый раз при конфигурации приложения:
+# когда изменился код программы и перезагрузилась страница
+
 configure do
+	# инициализация БД
 	init_db
 	@db.execute 'CREATE TABLE IF NOT EXISTS Posts
 	(
@@ -27,11 +33,18 @@ get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
 
+# обработчик get-запроса /new 
+# (браузер получает страницу с сервера)
 get '/new' do
  erb :new
 end
 
+# обработчик post-запроса /new 
+# (браузер получает страницу с сервера)
+
 post '/new' do
+	# получаем переменную из post - запроса
+
 	content = params[:content]
 	erb "You typed: #{content}"
 end
