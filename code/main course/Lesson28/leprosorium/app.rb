37bafe6867625911ecb2d8c47d007c8c4f6a805e
Aleksandr <sas2job@gmail.com>
2019-02-01 18:37:24 +0300

    Rename /post/... to /details/... and add /details/... handler

--- code/main course/Lesson28/leprosorium/app.rb
+++ code/main course/Lesson28/leprosorium/app.rb
@@ -64,5 +64,13 @@ post '/new' do
 	# перенаправление на главную страницу
 
 	redirect to '/'
-	erb "You typed: #{content}"
+	# erb "You typed: #{content}"
+end
+
+# вывод информации о посте
+
+get '/details/:post_id' do
+	post_id = params[:post_id]
+
+	erb "Displaying information for post with id #{post_id}"
 end
