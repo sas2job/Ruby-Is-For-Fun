3b33a1456e8952694e51300a05c65aba6aaed034
Alex Smirnov <sas2job@gmail.com>
2019-03-13 18:34:25 +0300

    Correction

--- code/main course/Lesson32/PizzaShop/public/app.js
+++ code/main course/Lesson32/PizzaShop/public/app.js
@@ -1,9 +1,9 @@
 function something(){
-    var x = window.localStorage.getItem('aaa');
+    var x = window.localStorage.getItem('bbb');
 
-    x = x + 1;
+    x = x * 1 + 1;
 
-    window.localStorage.setItem('aaa', 555);
+    window.localStorage.setItem('bbb', 555);
     
     alert(x);
 }
