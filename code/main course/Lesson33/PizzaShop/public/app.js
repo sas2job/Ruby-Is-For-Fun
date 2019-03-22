c7f45044703330367273a5df1dc3288e95186557
Alex Smirnov <sas2job@gmail.com>
2019-03-22 07:54:13 +0300

    Counter in localStorage

--- code/main course/Lesson33/PizzaShop/public/app.js
+++ code/main course/Lesson33/PizzaShop/public/app.js
@@ -1,9 +1,9 @@
 function something(){
-    var x = window.localStorage.getItem('bbb');
+    var x = window.localStorage.getItem('bbb'); // x = hh['bbb']
 
-    x = x * 1 + 1;
+    x = x * 1 + 1;  // x = x + 1
 
-    window.localStorage.setItem('bbb', 555);
+    window.localStorage.setItem('bbb', x);    // hh['bbb'] = x
     
     alert(x);
 }
