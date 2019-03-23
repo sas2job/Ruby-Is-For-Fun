b81a937c40fb7451fcae8676ae888d1e45412510
Alex Smirnov <sas2job@gmail.com>
2019-03-23 20:25:56 +0300

    Keep amount of products

--- code/main course/Lesson33/PizzaShop/public/app.js
+++ code/main course/Lesson33/PizzaShop/public/app.js
@@ -9,5 +9,9 @@ function something(){
 }
 
 function add_to_cart(id){
-    alert('You added pizza with id: ' + id);
+    var x = window.localStorage.getItem('product_' + id);
+    
+    x = x * 1 + 1;
+    
+    window.localStorage.setItem('product_' + id, x);
 }
