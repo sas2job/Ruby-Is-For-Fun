28a9224894a4455ec920145ca34246d1a836bbe0
Alex Smirnov <sas2job@gmail.com>
2019-03-23 20:32:44 +0300

    Update  add_to_cart function

--- code/main course/Lesson33/PizzaShop/public/app.js
+++ code/main course/Lesson33/PizzaShop/public/app.js
@@ -13,8 +13,6 @@ function add_to_cart(id){
     var key = 'product_' + id
 
     var x = window.localStorage.getItem(key);
-    
     x = x * 1 + 1;
-    
     window.localStorage.setItem(key, x);
 }
