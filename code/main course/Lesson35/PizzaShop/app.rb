d7fc7d24abbc7bf7d85cbed89292c8e2ecf30b37
Alex Smirnov <sas2job@gmail.com>
2019-03-30 22:02:00 +0300

    Lesson 35 - Init app

---
+++ code/main course/Lesson35/PizzaShop/app.rb
@@ -0,0 +1,26 @@
+#encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:pizzashop.db"
+
+class Product < ActiveRecord::Base
+	
+end
+
+
+
+get '/' do
+	@products = Product.all
+	erb :index		
+end
+
+get '/about' do
+	erb :about		
+end
+
+post '/cart' do
+	erb 'Hello!'		
+end
---
+++ code/main course/Lesson35/PizzaShop/public/app.js
@@ -0,0 +1,71 @@
+function something(){
+    var x = window.localStorage.getItem('bbb'); // x = hh['bbb']
+
+    x = x * 1 + 1;  // x = x + 1
+
+    window.localStorage.setItem('bbb', x);    // hh['bbb'] = x
+    
+    alert(x);
+}
+
+function add_to_cart(id){
+    
+    var key = 'product_' + id
+
+    var x = window.localStorage.getItem(key);
+    x = x * 1 + 1;
+    window.localStorage.setItem(key, x);
+
+    // вывод количества items в корзине
+    // alert('Items in your cart: ' + cart_get_number_of_items());
+    update_orders_input();
+    update_orders_button();
+}
+
+function update_orders_input(){
+  
+  var orders = cart_get_orders();
+  $('#orders_input').val(orders);
+
+}
+
+function update_orders_button(){
+
+  var text = 'Cart (' + cart_get_number_of_items() + ')';
+  $('#orders_button').val(text);
+}
+function cart_get_number_of_items() {
+
+  var cnt = 0;
+
+  for (var i = 0; i < window.localStorage.length; i++){
+    
+    var key = window.localStorage.key(i); // получаем ключ
+    var value = window.localStorage.getItem(key); // получаем значение, аналог в ruby: hh[key] = x 
+
+    if(key.indexOf('product_') == 0)
+    {
+      cnt = cnt + value * 1;
+    }
+  }
+
+  return cnt;
+}
+
+function cart_get_orders() {
+
+  var orders = '';
+
+  for (var i = 0; i < window.localStorage.length; i++){
+    
+    var key = window.localStorage.key(i); // получаем ключ
+    var value = window.localStorage.getItem(key); // получаем значение, аналог в ruby: hh[key] = x 
+
+    if(key.indexOf('product_') == 0)
+    {
+      orders = orders + key + '=' + value + ',';
+    }
+  }
+
+  return orders;
+}
